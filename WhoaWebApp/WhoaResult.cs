b91f52bfd21f5d07c21942689c21518f0cabad4b
Patryk Szymanski <szypat3110@gmail.com>
2025-11-26 17:15:50 +0000

    Basic app set up, default functions not removed yet, random whoa working

---
+++ WhoaWebApp/WhoaResult.cs
@@ -0,0 +1,19 @@
+﻿namespace WhoaWebApp
+{
+    public class WhoaResult
+    {
+        public string? movie { get; set; }
+        public int year { get; set; }
+        public string? release_date { get; set; }
+        public string? director { get; set; }
+        public string? character { get; set; }
+        public string? movie_duration { get; set; }
+        public string? timestamp { get; set; }
+        public string? full_line { get; set; }
+        public int current_whoa_in_movie { get; set; }
+        public int total_whoas_in_movie { get; set; }
+        public Dictionary<string, string> video { get; set; }
+        public string? poster { set; get; }
+        public string? audio { get; set; }
+    }
+}
