1ae28e45207f2bc8f3e699614190fcbe0efd8941
Patryk Szymanski <szypat3110@gmail.com>
2025-12-09 14:48:34 +0000

    Replaced all instances of Name with NameString to try get tests to compile in pipeline

--- WhoaWebApp.E2ETests/WhoaAppE2E.cs
+++ WhoaWebApp.E2ETests/WhoaAppE2E.cs
@@ -19,16 +19,16 @@ namespace WhoaWebApp.E2ETests
             var context = await browser.NewContextAsync();
             var page = await context.NewPageAsync();
             await page.GotoAsync("https://patrykszymanski31.github.io/CA3_Blazor_X00201937/");
-            await page.GetByRole(AriaRole.Heading, new() { Name = "Whoa! Hey there!" }).ClickAsync();
-            await page.GetByRole(AriaRole.Link, new() { Name = " Random Whoa" }).ClickAsync();
+            await page.GetByRole(AriaRole.Heading, new() { NameString = "Whoa! Hey there!" }).ClickAsync();
+            await page.GetByRole(AriaRole.Link, new() { NameString = " Random Whoa" }).ClickAsync();
             await page.WaitForURLAsync("https://patrykszymanski31.github.io/CA3_Blazor_X00201937/random-whoa");
-            await page.GetByRole(AriaRole.Heading, new() { Name = "Random Keanu \"Whoa\"" }).ClickAsync();
+            await page.GetByRole(AriaRole.Heading, new() { NameString = "Random Keanu \"Whoa\"" }).ClickAsync();
             await page.GetByText("Character:").ClickAsync();
-            await page.GetByRole(AriaRole.Heading, new() { Name = "Movie Details with OMDb" }).ClickAsync();
+            await page.GetByRole(AriaRole.Heading, new() { NameString = "Movie Details with OMDb" }).ClickAsync();
             await page.GetByText("IMDb rating:").ClickAsync();
-            await page.GetByRole(AriaRole.Button, new() { Name = "Get another whoa" }).ClickAsync();
+            await page.GetByRole(AriaRole.Button, new() { NameString = "Get another whoa" }).ClickAsync();
             await page.GetByText("Character:").ClickAsync();
-            await page.GetByRole(AriaRole.Heading, new() { Name = "Movie Details with OMDb" }).ClickAsync();
+            await page.GetByRole(AriaRole.Heading, new() { NameString = "Movie Details with OMDb" }).ClickAsync();
 
         }  
 
@@ -43,16 +43,16 @@ namespace WhoaWebApp.E2ETests
             var context = await browser.NewContextAsync();
             var page = await context.NewPageAsync();
             await page.GotoAsync("https://patrykszymanski31.github.io/CA3_Blazor_X00201937/");
-            await page.GetByRole(AriaRole.Heading, new() { Name = "Whoa! Hey there!" }).ClickAsync();
-            await page.GetByRole(AriaRole.Link, new() { Name = " Search Whoas" }).ClickAsync();
+            await page.GetByRole(AriaRole.Heading, new() { NameString = "Whoa! Hey there!" }).ClickAsync();
+            await page.GetByRole(AriaRole.Link, new() { NameString = " Search Whoas" }).ClickAsync();
             await page.WaitForURLAsync("https://patrykszymanski31.github.io/CA3_Blazor_X00201937/whoa-search");
             await page.GetByText("Search Whoas by Movie Number of results Between 1 and 30 results. Movie Babes in").ClickAsync();
             await page.GetByLabel("Number of results").ClickAsync();
             await page.GetByLabel("Number of results").FillAsync("10");
-            await page.GetByRole(AriaRole.Combobox, new() { Name = "Movie" }).SelectOptionAsync(new[] { "Bill & Ted's Excellent Adventure" });
-            await page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+            await page.GetByRole(AriaRole.Combobox, new() { NameString = "Movie" }).SelectOptionAsync(new[] { "Bill & Ted's Excellent Adventure" });
+            await page.GetByRole(AriaRole.Button, new() { NameString = "Search" }).ClickAsync();
             await page.Locator("strong").First.ClickAsync();
-            await page.GetByRole(AriaRole.Heading, new() { Name = "Movie details with OMDb" }).ClickAsync();
+            await page.GetByRole(AriaRole.Heading, new() { NameString = "Movie details with OMDb" }).ClickAsync();
             await page.GetByText("Title: Bill & Ted's Excellent Adventure (1989)").ClickAsync();
         }
     }
